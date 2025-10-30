f09d211ec5a6afd5af575d99c2bddab682c92414
ccui <ccui@princeton.edu>
2025-10-30 14:29:10 -0400

    Generalize TeamThreadMDRangeIterator

--- include/execution/team_thread_md_range_iterator.hpp
+++ include/execution/team_thread_md_range_iterator.hpp
@@ -1,43 +1,30 @@
 #pragma once
 
-#include "datatypes/impl/register_array.hpp"
 #include "macros.hpp"
 #include "policy.hpp"
 #include "void_iterator.hpp"
 #include <Kokkos_Core.hpp>
 #include <type_traits>
 
+// template <typename ExectutionSpace, typename Enable = void>
+// class is_host_space;
+
+// #if defined(KOKKOS_ENABLE_SERIAL)
+// template <>
+// class is_host_space<Kokkos::Serial> {
+// constexpr static bool value = true;
+// };
+// #endif
+
+// #if defined(KOKKOS_ENABLE_OPENMP)
+// template <>
+// class is_host_space<Kokkos::OpenMP> {
+// constexpr static bool value = true;
+// };
+// #endif
+
 namespace specfem {
 namespace execution {
-
-namespace impl {
-/**
- * @brief Base template for N-dimensional range iterator implementation details
- * @tparam Rank The dimensionality of the iterator
- */
-template <int Rank> struct TeamThreadMDRangeIteratorN {};
-
-/**
- * @brief Specialization for 2D range iterator
- */
-template <> struct TeamThreadMDRangeIteratorN<2> {
-  int n1_; ///< Size of first dimension
-  int n2_; ///< Size of second dimension
-  TeamThreadMDRangeIteratorN(int n1, int n2) : n1_(n1), n2_(n2) {}
-};
-
-/**
- * @brief Specialization for 3D range iterator
- */
-template <> struct TeamThreadMDRangeIteratorN<3> {
-  int n1_; ///< Size of first dimension
-  int n2_; ///< Size of second dimension
-  int n3_; ///< Size of third dimension
-  TeamThreadMDRangeIteratorN(int n1, int n2, int n3)
-      : n1_(n1), n2_(n2), n3_(n3) {}
-};
-} // namespace impl
-
 /**
  * @brief Multi-dimensional range iterator for team-based parallel execution
  *
@@ -51,12 +38,11 @@ template <> struct TeamThreadMDRangeIteratorN<3> {
  */
 template <typename TeamMemberType, int Rank>
 class TeamThreadMDRangeIterator
-    : public TeamThreadRangePolicy<TeamMemberType, int>,
-      private impl::TeamThreadMDRangeIteratorN<Rank> {
+    : public TeamThreadRangePolicy<TeamMemberType, int> {
 
 private:
   using base_type = TeamThreadRangePolicy<TeamMemberType, int>;
-  using n_type = impl::TeamThreadMDRangeIteratorN<Rank>;
+  int extents_[Rank]; ///< Extents of each dimension
 
 public:
   constexpr static auto rank = Rank; ///< Rank of the iterator (2 or 3)
@@ -80,72 +66,79 @@ public:
    * @param i Linear index
    * @return index_type 2D / 3D coordinates
    */
+  template <
+      typename T = TeamMemberType,
+      std::enable_if_t<
+          std::is_same_v<typename TeamMemberType::execution_space::memory_space,
+                         Kokkos::HostSpace>,
+          int> = 0>
   KOKKOS_INLINE_FUNCTION const index_type operator()(const int &i) const {
-    if constexpr (Rank == 2) {
-      const int &n1 = n_type::n1_;
-      const int &n2 = n_type::n2_;
-      if constexpr (std::is_same_v<typename TeamMemberType::execution_space,
-                                   Kokkos::DefaultHostExecutionSpace>) {
-        const int i1 = i % n1;
-        const int i2 = i / n1;
-        return index_type(i1, i2);
-      } else {
-        const int i1 = i / n2;
-        const int i2 = i % n2;
-        return index_type(i1, i2);
-      }
-    } else if constexpr (Rank == 3) {
-      const int &n1 = n_type::n1_;
-      const int &n2 = n_type::n2_;
-      const int &n3 = n_type::n3_;
-      if constexpr (std::is_same_v<typename TeamMemberType::execution_space,
-                                   Kokkos::DefaultHostExecutionSpace>) {
-        const int i1 = i % n1;
-        const int i2 = (i / n1) % n2;
-        const int i3 = i / (n1 * n2);
-        return index_type(i1, i2, i3);
-      } else {
-        const int i1 = i / (n2 * n3);
-        const int i2 = (i / n3) % n2;
-        const int i3 = i % n3;
-        return index_type(i1, i2, i3);
-      }
+    index_type result;
+    result(0) = i % extents_[0];
+    int temp = i / extents_[0];
+
+    for (int r = 1; r < rank - 1; ++r) {
+      result(r) = temp % extents_[r];
+      temp /= extents_[r];
     }
+
+    if constexpr (rank > 1) {
+      result(rank - 1) = temp;
+    }
+    return result;
   }
 
-  /**
-   * @brief Constructor for 2D TeamThreadMDRangeIterator
-   *
-   * @param team The Kokkos team member
-   * @param n1 Size of first dimension
-   * @param n2 Size of second dimension
-   */
-  template <int R = Rank, typename std::enable_if_t<R == 2, int> = 0>
-  KOKKOS_INLINE_FUNCTION TeamThreadMDRangeIterator(const TeamMemberType &team,
-                                                   int n1, int n2)
-      : base_type(team, n1 * n2), n_type{ n1, n2 } {}
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
 
-  /**
-   * @brief Constructor for 3D TeamThreadMDRangeIterator
-   *
-   * @param team The Kokkos team member
-   * @param n1 Size of first dimension
-   * @param n2 Size of second dimension
-   * @param n3 Size of third dimension
-   */
-  template <int R = Rank, typename std::enable_if_t<R == 3, int> = 0>
-  KOKKOS_INLINE_FUNCTION TeamThreadMDRangeIterator(const TeamMemberType &team,
-                                                   int n1, int n2, int n3)
-      : base_type(team, n1 * n2 * n3), n_type{ n1, n2, n3 } {}
+private:
+#ifdef KOKKOS_ENABLE_CUDA
+  using device_memory_space = Kokkos::CudaSpace;
+#elif defined(KOKKOS_ENABLE_HIP)
+  using device_memory_space = Kokkos::HIPSpace;
+#endif
+public:
+  template <
+      typename T = TeamMemberType,
+      std::enable_if_t<
+          std::is_same_v<typename TeamMemberType::execution_space::memory_space,
+                         device_memory_space>,
+          int> = 0>
+  KOKKOS_INLINE_FUNCTION const index_type operator()(const int &i) const {
+    index_type result;
+    result(rank - 1) = i % extents_[rank - 1];
+    int temp = i / extents_[rank - 1];
+
+    for (int r = rank - 2; r > 0; --r) {
+      result(r) = temp % extents_[r];
+      temp /= extents_[r];
+    }
+
+    if constexpr (rank > 1) {
+      result(0) = temp;
+    }
+    return result;
+  }
+#endif
+
+  template <typename... Indices>
+  constexpr TeamThreadMDRangeIterator(const TeamMemberType &team,
+                                      Indices... indices)
+      : base_type(team,
+                  [&]() {
+                    size_t product = 1;
+                    ((product *= indices), ...);
+                    return product;
+                  }()),
+        extents_{ indices... } {}
 };
 
 // User-defined deduction guide
 template <typename TeamMemberType>
-TeamThreadMDRangeIterator(TeamMemberType, int, int)
+TeamThreadMDRangeIterator(TeamMemberType, size_t, size_t)
     -> TeamThreadMDRangeIterator<TeamMemberType, 2>;
 
 template <typename TeamMemberType>
-TeamThreadMDRangeIterator(TeamMemberType, int, int, int)
+TeamThreadMDRangeIterator(TeamMemberType, size_t, size_t, size_t)
     -> TeamThreadMDRangeIterator<TeamMemberType, 3>;
 
 } // namespace execution
