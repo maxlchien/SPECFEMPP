6ef02bc44502d0f0e9175be4fdbb8064df1ac0dd
Kentaro Hanson <kh3372@princeton.edu>
2025-12-22 14:18:37 -0500

    removed unused accessor pack

--- tests/unit-tests/utilities/include/fixture/nonconforming_interface.hpp
+++ tests/unit-tests/utilities/include/fixture/nonconforming_interface.hpp
@@ -4,10 +4,3 @@
 #include "nonconforming_interface/edge_function.hpp"
 #include "nonconforming_interface/quadrature.hpp"
 #include "nonconforming_interface/transfer_function.hpp"
-
-namespace specfem::test_fixture {
-
-// later: we will need to build accessor pack fixtures.
-struct AccessorPack2D {};
-
-} // namespace specfem::test_fixture
