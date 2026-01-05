a3fcbc757ab32c319a2e30640bfbcd35e3cf498b
ccui <ccui@princeton.edu>
2026-01-05 15:17:39 -0500

    Move macros to specfem/macros folder.

---
+++ core/specfem/macros/macros_impl/seq_cat_with_or.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <boost/preprocessor.hpp>
+
+// ------- Implemetation Details --------
+
+#define _SEQ_CAT_WITH_OR(seq)                                                  \
+  BOOST_PP_CAT(_SEQ_CAT_WITH_OR_, BOOST_PP_SEQ_SIZE(seq)) seq
+
+#define _SEQ_CAT_WITH_OR_1(x) x
+#define _SEQ_CAT_WITH_OR_2(x) x || _SEQ_CAT_WITH_OR_1
+#define _SEQ_CAT_WITH_OR_3(x) x || _SEQ_CAT_WITH_OR_2
+#define _SEQ_CAT_WITH_OR_4(x) x || _SEQ_CAT_WITH_OR_3
+#define _SEQ_CAT_WITH_OR_5(x) x || _SEQ_CAT_WITH_OR_4
+#define _SEQ_CAT_WITH_OR_6(x) x || _SEQ_CAT_WITH_OR_5
+#define _SEQ_CAT_WITH_OR_7(x) x || _SEQ_CAT_WITH_OR_6
+#define _SEQ_CAT_WITH_OR_8(x) x || _SEQ_CAT_WITH_OR_7
+#define _SEQ_CAT_WITH_OR_9(x) x || _SEQ_CAT_WITH_OR_8
+#define _SEQ_CAT_WITH_OR_10(x) x || _SEQ_CAT_WITH_OR_9
+#define _SEQ_CAT_WITH_OR_11(x) x || _SEQ_CAT_WITH_OR_10
+#define _SEQ_CAT_WITH_OR_12(x) x || _SEQ_CAT_WITH_OR_11
+#define _SEQ_CAT_WITH_OR_13(x) x || _SEQ_CAT_WITH_OR_12
+#define _SEQ_CAT_WITH_OR_14(x) x || _SEQ_CAT_WITH_OR_13
+#define _SEQ_CAT_WITH_OR_15(x) x || _SEQ_CAT_WITH_OR_14
+#define _SEQ_CAT_WITH_OR_16(x) x || _SEQ_CAT_WITH_OR_15
