63c0060aa05a7b05bf237ec4a597b6069aa93948
Lucas Sawade <lsawade@princeton.edu>
2026-01-13 16:10:15 -0500

    Updated the tests to be parameterized.

--- tests/unit-tests/compute_coupling/nonconforming/acoustic_elastic.cpp
+++ tests/unit-tests/compute_coupling/nonconforming/acoustic_elastic.cpp
@@ -5,37 +5,88 @@
 #include "utilities/include/fixture/nonconforming_interface/intersection_function.hpp"
 #include "utilities/include/fixture/nonconforming_interface/quadrature.hpp"
 
-template <typename TestingTypes>
-using ComputeCouplingTest2D_AcousticElastic_Natural = ComputeCouplingTestSuite<
-    specfem::interface::interface_tag::acoustic_elastic,
-    specfem::interface::flux_scheme_tag::natural>::
-    ComputeCouplingTest2D<TestingTypes>;
+#include <ostream>
+#include <string>
+#include <tuple>
+
+namespace {
 
 using namespace specfem::test_fixture;
 
-using TransferFunctionTestTypes2D_AcousticElastic_Natural =
-    ::testing::Types<std::tuple<
-        TransferFunctionInitializer2D::FromQuadratureRules<
-            QuadraturePoints::Asymm4Point, QuadraturePoints::Asymm5Point>,
-        IntersectionFunctionInitializer2D::FromAnalyticalFunction<
-            AnalyticalFunctionType::Vectorized<
-                AnalyticalFunctionType::Power<0>,
-                AnalyticalFunctionType::Power<1> >,
-            QuadraturePoints::Asymm5Point>,
-        EdgeFunctionInitializer2D::FromAnalyticalFunction<
-            AnalyticalFunctionType::Vectorized<
-                AnalyticalFunctionType::Power<1>,
-                AnalyticalFunctionType::Power<2> >,
-            QuadraturePoints::Asymm4Point>,
-        IntersectionFunctionInitializer2D::FromAnalyticalFunction<
-            AnalyticalFunctionType::Sum<AnalyticalFunctionType::Power<1>,
-                                        AnalyticalFunctionType::Power<3> >,
-            QuadraturePoints::Asymm5Point> > >;
+using Asymm4to5_VectorizedPower = std::tuple<
+    TransferFunctionInitializer2D::FromQuadratureRules<
+        QuadraturePoints::Asymm4Point, QuadraturePoints::Asymm5Point>,
+    IntersectionFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Vectorized<AnalyticalFunctionType::Power<0>,
+                                           AnalyticalFunctionType::Power<1> >,
+        QuadraturePoints::Asymm5Point>,
+    EdgeFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Vectorized<AnalyticalFunctionType::Power<1>,
+                                           AnalyticalFunctionType::Power<2> >,
+        QuadraturePoints::Asymm4Point>,
+    IntersectionFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Sum<AnalyticalFunctionType::Power<1>,
+                                    AnalyticalFunctionType::Power<3> >,
+        QuadraturePoints::Asymm5Point> >;
 
-TYPED_TEST_SUITE(ComputeCouplingTest2D_AcousticElastic_Natural,
-                 TransferFunctionTestTypes2D_AcousticElastic_Natural);
+// Helper to run test with specific types
+template <typename TestTypes> void run_acoustic_elastic_test() {
+  using TransferFunctionInit = std::tuple_element_t<0, TestTypes>;
+  using IntersectionNormalInit = std::tuple_element_t<1, TestTypes>;
+  using EdgeFunctionInit = std::tuple_element_t<2, TestTypes>;
+  using ExpectedSolutionInit = std::tuple_element_t<3, TestTypes>;
 
-TYPED_TEST(ComputeCouplingTest2D_AcousticElastic_Natural,
-           ExecuteImplComputeCoupling) {
-  this->run_test();
+  specfem::test_fixture::TransferFunction2D<TransferFunctionInit>
+      transfer_function{ TransferFunctionInit{} };
+  specfem::test_fixture::IntersectionFunction2D<IntersectionNormalInit>
+      intersection_normal{ IntersectionNormalInit{} };
+  specfem::test_fixture::EdgeFunction2D<EdgeFunctionInit> edge_function{
+    EdgeFunctionInit{}
+  };
+  specfem::test_fixture::IntersectionFunction2D<ExpectedSolutionInit>
+      expected_solution{ ExpectedSolutionInit{} };
+
+  execute_impl_compute_coupling<
+      specfem::interface::interface_tag::acoustic_elastic,
+      EdgeFunctionAccessor<
+          specfem::interface::interface_tag::acoustic_elastic> >(
+      transfer_function, intersection_normal, edge_function, expected_solution);
 }
+
+enum class AcousticElasticCase {
+  Asymm4to5_VectorizedPower,
+};
+
+struct AcousticElasticParams {
+  AcousticElasticCase which;
+  const char *name;
+};
+
+void PrintTo(const AcousticElasticParams &, std::ostream *os) { *os << ""; }
+
+std::ostream &operator<<(std::ostream &os,
+                         const AcousticElasticParams &params) {
+  return os << params.name;
+}
+
+class NonconformingAcousticElasticTest
+    : public ::testing::TestWithParam<AcousticElasticParams> {};
+
+TEST_P(NonconformingAcousticElasticTest, ComputeCoupling) {
+  switch (GetParam().which) {
+  case AcousticElasticCase::Asymm4to5_VectorizedPower:
+    run_acoustic_elastic_test<Asymm4to5_VectorizedPower>();
+    return;
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Polynomial, NonconformingAcousticElasticTest,
+    ::testing::Values(AcousticElasticParams{
+        AcousticElasticCase::Asymm4to5_VectorizedPower,
+        "Asymm4to5_VectorizedPower",
+    }),
+    [](const ::testing::TestParamInfo<AcousticElasticParams> &info)
+        -> std::string { return info.param.name; });
+
+} // anonymous namespace
--- tests/unit-tests/compute_coupling/nonconforming/elastic_acoustic.cpp
+++ tests/unit-tests/compute_coupling/nonconforming/elastic_acoustic.cpp
@@ -5,49 +5,120 @@
 #include "utilities/include/fixture/nonconforming_interface/intersection_function.hpp"
 #include "utilities/include/fixture/nonconforming_interface/quadrature.hpp"
 
-template <typename TestingTypes>
-using ComputeCouplingTest2D_ElasticAcoustic_Natural = ComputeCouplingTestSuite<
-    specfem::interface::interface_tag::elastic_acoustic,
-    specfem::interface::flux_scheme_tag::natural>::
-    ComputeCouplingTest2D<TestingTypes>;
+#include <cctype>
+#include <ostream>
+#include <string>
+#include <tuple>
+
+namespace {
 
 using namespace specfem::test_fixture;
 
-using TransferFunctionTestTypes2D_ElasticAcoustic_Natural = ::testing::Types<
-    std::tuple<TransferFunctionInitializer2D::FromQuadratureRules<
-                   QuadraturePoints::GLL2, QuadraturePoints::GLL2>,
-               IntersectionFunctionInitializer2D::FromAnalyticalFunction<
-                   AnalyticalFunctionType::Vectorized<
-                       AnalyticalFunctionType::Power<0>,
-                       AnalyticalFunctionType::Power<1> >,
-                   QuadraturePoints::GLL2>,
-               EdgeFunctionInitializer2D::FromAnalyticalFunction<
-                   AnalyticalFunctionType::Power<0>, QuadraturePoints::GLL2>,
-               IntersectionFunctionInitializer2D::FromAnalyticalFunction<
-                   AnalyticalFunctionType::Vectorized<
-                       AnalyticalFunctionType::Power<0>,
-                       AnalyticalFunctionType::Power<1> >,
-                   QuadraturePoints::GLL2> >,
-    std::tuple<
-        TransferFunctionInitializer2D::FromQuadratureRules<
-            QuadraturePoints::Asymm4Point, QuadraturePoints::Asymm5Point>,
-        IntersectionFunctionInitializer2D::FromAnalyticalFunction<
-            AnalyticalFunctionType::Vectorized<
-                AnalyticalFunctionType::Power<3>,
-                AnalyticalFunctionType::Power<2> >,
-            QuadraturePoints::Asymm5Point>,
-        EdgeFunctionInitializer2D::FromAnalyticalFunction<
-            AnalyticalFunctionType::Power<1>, QuadraturePoints::Asymm4Point>,
-        IntersectionFunctionInitializer2D::FromAnalyticalFunction<
-            AnalyticalFunctionType::Vectorized<
-                AnalyticalFunctionType::Power<4>,
-                AnalyticalFunctionType::Power<3> >,
-            QuadraturePoints::Asymm5Point> > >;
+using GLL2_Constant = std::tuple<
+    TransferFunctionInitializer2D::FromQuadratureRules<QuadraturePoints::GLL2,
+                                                       QuadraturePoints::GLL2>,
+    IntersectionFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Vectorized<AnalyticalFunctionType::Power<0>,
+                                           AnalyticalFunctionType::Power<1> >,
+        QuadraturePoints::GLL2>,
+    EdgeFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Power<0>, QuadraturePoints::GLL2>,
+    IntersectionFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Vectorized<AnalyticalFunctionType::Power<0>,
+                                           AnalyticalFunctionType::Power<1> >,
+        QuadraturePoints::GLL2> >;
 
-TYPED_TEST_SUITE(ComputeCouplingTest2D_ElasticAcoustic_Natural,
-                 TransferFunctionTestTypes2D_ElasticAcoustic_Natural);
+using Asymm4to5_HigherOrder = std::tuple<
+    TransferFunctionInitializer2D::FromQuadratureRules<
+        QuadraturePoints::Asymm4Point, QuadraturePoints::Asymm5Point>,
+    IntersectionFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Vectorized<AnalyticalFunctionType::Power<3>,
+                                           AnalyticalFunctionType::Power<2> >,
+        QuadraturePoints::Asymm5Point>,
+    EdgeFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Power<1>, QuadraturePoints::Asymm4Point>,
+    IntersectionFunctionInitializer2D::FromAnalyticalFunction<
+        AnalyticalFunctionType::Vectorized<AnalyticalFunctionType::Power<4>,
+                                           AnalyticalFunctionType::Power<3> >,
+        QuadraturePoints::Asymm5Point> >;
 
-TYPED_TEST(ComputeCouplingTest2D_ElasticAcoustic_Natural,
-           ExecuteImplComputeCoupling) {
-  this->run_test();
+template <typename TestTypes> void run_elastic_acoustic_test() {
+  using TransferFunctionInit = std::tuple_element_t<0, TestTypes>;
+  using IntersectionNormalInit = std::tuple_element_t<1, TestTypes>;
+  using EdgeFunctionInit = std::tuple_element_t<2, TestTypes>;
+  using ExpectedSolutionInit = std::tuple_element_t<3, TestTypes>;
+
+  specfem::test_fixture::TransferFunction2D<TransferFunctionInit>
+      transfer_function{ TransferFunctionInit{} };
+  specfem::test_fixture::IntersectionFunction2D<IntersectionNormalInit>
+      intersection_normal{ IntersectionNormalInit{} };
+  specfem::test_fixture::EdgeFunction2D<EdgeFunctionInit> edge_function{
+    EdgeFunctionInit{}
+  };
+  specfem::test_fixture::IntersectionFunction2D<ExpectedSolutionInit>
+      expected_solution{ ExpectedSolutionInit{} };
+
+  execute_impl_compute_coupling<
+      specfem::interface::interface_tag::elastic_acoustic,
+      EdgeFunctionAccessor<
+          specfem::interface::interface_tag::elastic_acoustic> >(
+      transfer_function, intersection_normal, edge_function, expected_solution);
 }
+
+enum class ElasticAcousticCase {
+  GLL2_Constant,
+  Asymm4to5_HigherOrder,
+};
+
+struct ElasticAcousticParams {
+  ElasticAcousticCase which;
+  const char *name;
+};
+
+void PrintTo(const ElasticAcousticParams &, std::ostream *os) { *os << ""; }
+
+std::ostream &operator<<(std::ostream &os,
+                         const ElasticAcousticParams &params) {
+  return os << params.name;
+}
+
+class NonconformingElasticAcousticTest
+    : public ::testing::TestWithParam<ElasticAcousticParams> {};
+
+TEST_P(NonconformingElasticAcousticTest, ComputeCoupling) {
+  switch (GetParam().which) {
+  case ElasticAcousticCase::GLL2_Constant:
+    run_elastic_acoustic_test<GLL2_Constant>();
+    return;
+  case ElasticAcousticCase::Asymm4to5_HigherOrder:
+    run_elastic_acoustic_test<Asymm4to5_HigherOrder>();
+    return;
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Polynomial, NonconformingElasticAcousticTest,
+    ::testing::Values(ElasticAcousticParams{ ElasticAcousticCase::GLL2_Constant,
+                                             "GLL2_Constant" },
+                      ElasticAcousticParams{
+                          ElasticAcousticCase::Asymm4to5_HigherOrder,
+                          "Asymm4to5_HigherOrder" }),
+    [](const ::testing::TestParamInfo<ElasticAcousticParams> &info)
+        -> std::string {
+      const std::string name = info.param.name;
+      std::string out;
+      out.reserve(name.size());
+      for (unsigned char ch : name) {
+        out.push_back(std::isalnum(ch) ? static_cast<char>(ch) : '_');
+      }
+      if (out.empty()) {
+        out = "Case";
+      }
+      const unsigned char first = static_cast<unsigned char>(out.front());
+      if (!(std::isalpha(first) || out.front() == '_')) {
+        out.insert(out.begin(), '_');
+      }
+      return out;
+    });
+
+} // namespace
