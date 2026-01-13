306f7d7843c699aff2b8a382635d65afe29acf5f
ccui <ccui@princeton.edu>
2026-01-13 12:30:53 -0500

    Document stacey details

--- docs/sections/api/boundary_conditions/stacey.rst
+++ docs/sections/api/boundary_conditions/stacey.rst
@@ -6,3 +6,11 @@
 .. doxygenfunction:: specfem::boundary_conditions::impl_apply_boundary_conditions(const stacey_type &, const PointBoundaryType &boundary, const PointPropertyType &property, const PointFieldType &field, PointAccelerationType &acceleration)
 
 .. doxygenfunction:: specfem::boundary_conditions::impl_compute_mass_matrix_terms(const stacey_type &, const type_real dt, const PointBoundaryType &boundary, const PointPropertyType &property, PointMassMatrixType &mass_matrix)
+
+Implementation Details
+^^^^^^^^^^^^^^^^^^^^^^
+
+.. doxygenfunction:: specfem::boundary_conditions::impl::impl_base_elastic_psv_traction
+.. doxygenfunction:: specfem::boundary_conditions::impl::impl_base_elastic_sh_traction
+.. doxygenfunction:: specfem::boundary_conditions::impl::impl_base_elastic_psv_t_traction
+.. doxygenfunction:: specfem::boundary_conditions::impl::impl_enforce_traction
--- include/boundary_conditions/stacey/stacey.hpp
+++ include/boundary_conditions/stacey/stacey.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "enumerations/boundary.hpp"
+#include "enumerations/medium.hpp"
 #include "specfem_setup.hpp"
 #include <Kokkos_Core.hpp>
 
@@ -16,6 +17,164 @@ namespace boundary_conditions {
 using stacey_type =
     std::integral_constant<specfem::element::boundary_tag,
                            specfem::element::boundary_tag::stacey>;
+
+namespace impl {
+
+/**
+ * @brief Apply Stacey absorbing boundary conditions for Elastic P-SV medium
+ *
+ * The traction vector is computed based on P-SV separation:
+ * \f[
+ * \mathbf{t} = - \rho V_p (\mathbf{v} \cdot \mathbf{n}) \mathbf{n} - \rho V_s
+ * (\mathbf{v} - (\mathbf{v} \cdot \mathbf{n}) \mathbf{n})
+ * \f]
+ *
+ * @tparam PointBoundaryType Point boundary type
+ * @tparam PointPropertyType Point property type
+ * @tparam PointVelocityType Point velocity type
+ * @tparam ViewType View type
+ * @param boundary Boundary object
+ * @param property Property object
+ * @param velocity Velocity object
+ * @param traction Traction object to be updated
+ */
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<!PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_base_elastic_psv_traction(
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_base_elastic_psv_traction(
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+/**
+ * @brief Apply Stacey absorbing boundary conditions for Elastic SH medium
+ *
+ * For SH waves (scalar anti-plane shear), the traction is:
+ * \f[
+ * t = - \rho V_s v
+ * \f]
+ *
+ * @tparam PointBoundaryType Point boundary type
+ * @tparam PointPropertyType Point property type
+ * @tparam PointVelocityType Point velocity type
+ * @tparam ViewType View type
+ * @param boundary Boundary object
+ * @param property Property object
+ * @param velocity Velocity object
+ * @param traction Traction object to be updated
+ */
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<!PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_base_elastic_sh_traction(
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_base_elastic_sh_traction(
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+/**
+ * @brief Apply Stacey absorbing boundary conditions for Elastic Cosserat
+ * (P-SV-t) medium
+ *
+ * Updates traction for both translational and rotational components.
+ *
+ * Translational part (P-SV):
+ * \f[
+ * \mathbf{t}_{trans} = - \rho V_p (\mathbf{v}_{trans} \cdot \mathbf{n})
+ * \mathbf{n} - \rho V_s (\mathbf{v}_{trans} - (\mathbf{v}_{trans} \cdot
+ * \mathbf{n}) \mathbf{n})
+ * \f]
+ *
+ * Rotational part (Twist):
+ * \f[
+ * t_{rot} = - J V_t \omega
+ * \f]
+ * where \f$J\f$ is the micro-inertia (polar moment of inertia times density)
+ * and \f$V_t\f$ is the twisting velocity.
+ *
+ * @tparam PointBoundaryType Point boundary type
+ * @tparam PointPropertyType Point property type
+ * @tparam PointVelocityType Point velocity type
+ * @tparam ViewType View type
+ * @param boundary Boundary object
+ * @param property Property object
+ * @param velocity Velocity object
+ * @param traction Traction object to be updated
+ */
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<!PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_base_elastic_psv_t_traction(
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_base_elastic_psv_t_traction(
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+/**
+ * @brief Apply Stacey absorbing boundary conditions for Acoustic Isotropic
+ * medium
+ *
+ * The boundary condition applied to the potential (or pressure) field is:
+ * \f[
+ * t = - \frac{1}{\rho V_p} v
+ * \f]
+ *
+ * @tparam PointBoundaryType Point boundary type
+ * @tparam PointPropertyType Point property type
+ * @tparam PointVelocityType Point velocity type
+ * @tparam ViewType View type
+ * @param boundary Boundary object
+ * @param property Property object
+ * @param velocity Velocity object
+ * @param traction Traction object to be updated
+ */
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<!PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_enforce_traction(
+    const std::integral_constant<specfem::element::medium_tag,
+                                 specfem::element::medium_tag::acoustic> &,
+    const std::integral_constant<specfem::element::property_tag,
+                                 specfem::element::property_tag::isotropic> &,
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+template <
+    typename PointBoundaryType, typename PointPropertyType,
+    typename PointVelocityType, typename ViewType,
+    typename std::enable_if_t<PointBoundaryType::simd::using_simd, int> = 0>
+KOKKOS_FUNCTION void impl_enforce_traction(
+    const std::integral_constant<specfem::element::medium_tag,
+                                 specfem::element::medium_tag::acoustic> &,
+    const std::integral_constant<specfem::element::property_tag,
+                                 specfem::element::property_tag::isotropic> &,
+    const PointBoundaryType &boundary, const PointPropertyType &property,
+    const PointVelocityType &velocity, ViewType &traction);
+
+} // namespace impl
 
 /**
  * @brief Apply Stacey absorbing boundary conditions
